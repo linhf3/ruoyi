346df6f7675d9d347a5d7f9a9cfbaa95901f51a7
linhf3 <191663059@qq.com>
2024-07-29 22:11:42 +0800

    新增期货页面

--- ruoyi-ui/vue.config.js
+++ ruoyi-ui/vue.config.js
@@ -35,7 +35,8 @@ module.exports = {
     proxy: {
       // detail: https://cli.vuejs.org/config/#devserver-proxy
       [process.env.VUE_APP_BASE_API]: {
-        target: `http://localhost:8080`,
+        //target: `http://localhost:8080`,
+        target: `http://124.71.0.29:8080`,
         changeOrigin: true,
         pathRewrite: {
           ['^' + process.env.VUE_APP_BASE_API]: ''
