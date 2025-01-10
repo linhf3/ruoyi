7ed715e99df545882cdf53a8c66720027c1cd1d5
linhongfei03 <191663059@qq.com>
2025-01-10 20:47:25 +0800

    更新

--- ruoyi-security/src/main/java/com/ruoyi/security/task/TbSecuritiesDataTask.java
+++ ruoyi-security/src/main/java/com/ruoyi/security/task/TbSecuritiesDataTask.java
@@ -15,6 +15,7 @@ import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.Future;
+import java.util.concurrent.locks.ReentrantLock;
 
 
 @Component("tbSecuritiesDataTask")
@@ -33,55 +34,72 @@ public class TbSecuritiesDataTask {
     @Autowired
     private ThreadPoolTaskExecutor taskExecutor;
 
-    public void execute(){
-        // 获取当前时间
-        LocalTime now = LocalTime.now();
-        // 定义目标时间 11:35
-        LocalTime targetTime1 = LocalTime.of(9, 00);
-        LocalTime targetTime2 = LocalTime.of(11, 31);
-        LocalTime targetTime3 = LocalTime.of(13, 30);
-        LocalTime targetTime4 = LocalTime.of(15, 01);
-        LocalTime targetTime5 = LocalTime.of(21, 00);
-        LocalTime targetTime6 = LocalTime.of(23, 00);
-        if ((now.isAfter(targetTime1) && now.isBefore(targetTime2)) || (now.isAfter(targetTime3) && now.isBefore(targetTime4))
-                || (now.isAfter(targetTime5) && now.isBefore(targetTime6))){
-            //1.查询有效配置
-            List<TbSecuritiesData> tbSecuritiesDataList = redisCache.getCacheList("tbSecuritiesDataList");
-            if (CollectionUtils.isEmpty(tbSecuritiesDataList)){
-                TbSecuritiesData tbSecuritiesData = new TbSecuritiesData();
-                tbSecuritiesData.setType(1);
-                tbSecuritiesData.setStatus(0);
-                tbSecuritiesDataList = tbSecuritiesDataMapper.selectTbSecuritiesDataList(tbSecuritiesData);
-                if (!CollectionUtils.isEmpty(tbSecuritiesDataList)){
-                    redisCache.setCacheList("tbSecuritiesDataList",tbSecuritiesDataList);
-                }
-            }
-            if (CollectionUtils.isEmpty(tbSecuritiesDataList)){
-                return;
-            }
-            List<Future<SecuritiesFutureVo>> futures = new ArrayList<>();
-            //2、构建线程
-            long startTime = System.currentTimeMillis();
-            for (TbSecuritiesData tbSecuritiesData : tbSecuritiesDataList) {
-                TbSecuritiesDataThread tbSecuritiesDataThread = new TbSecuritiesDataThread(tbSecuritiesData, coreAlgorithmContet);
-                Future<SecuritiesFutureVo> future = taskExecutor.submit(tbSecuritiesDataThread);
-                futures.add(future);
-            }
-            //3、等待所有任务完成
-            List<SecuritiesFutureVo> list = new ArrayList<>();
-            for (Future<SecuritiesFutureVo> future : futures) {
-                try {
-                    list.add(future.get());// 阻塞直到任务完成
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-            long endTime = System.currentTimeMillis();
-            log.debug("执行时长：{}", endTime - startTime);
-            //4、存到redis中
-            redisCache.setCacheList("securitiesFutureVoList",list);
-        }
+    ReentrantLock lock = new ReentrantLock();
 
+    public void execute(){
+        boolean b = lock.tryLock();
+        if (!b){
+            return;
+        }
+        try {
+            // 获取当前时间
+            LocalTime now = LocalTime.now();
+            // 定义目标时间 11:35
+            LocalTime targetTime1 = LocalTime.of(9, 00);
+            LocalTime targetTime2 = LocalTime.of(11, 31);
+            LocalTime targetTime3 = LocalTime.of(13, 30);
+            LocalTime targetTime4 = LocalTime.of(15, 01);
+            LocalTime targetTime5 = LocalTime.of(21, 00);
+            LocalTime targetTime6 = LocalTime.of(23, 00);
+            if ((now.isAfter(targetTime1) && now.isBefore(targetTime2)) || (now.isAfter(targetTime3) && now.isBefore(targetTime4))
+                    || (now.isAfter(targetTime5) && now.isBefore(targetTime6))){
+                //1.查询有效配置
+                List<TbSecuritiesData> tbSecuritiesDataList = redisCache.getCacheList("tbSecuritiesDataList");
+                if (CollectionUtils.isEmpty(tbSecuritiesDataList)){
+                    TbSecuritiesData tbSecuritiesData = new TbSecuritiesData();
+                    tbSecuritiesData.setType(1);
+                    tbSecuritiesData.setStatus(0);
+                    tbSecuritiesDataList = tbSecuritiesDataMapper.selectTbSecuritiesDataList(tbSecuritiesData);
+                    if (!CollectionUtils.isEmpty(tbSecuritiesDataList)){
+                        redisCache.setCacheList("tbSecuritiesDataList",tbSecuritiesDataList);
+                    }
+                }
+                if (CollectionUtils.isEmpty(tbSecuritiesDataList)){
+                    return;
+                }
+                List<Future<SecuritiesFutureVo>> futures = new ArrayList<>();
+                //2、构建线程
+                long startTime = System.currentTimeMillis();
+                for (TbSecuritiesData tbSecuritiesData : tbSecuritiesDataList) {
+                    TbSecuritiesDataThread tbSecuritiesDataThread = new TbSecuritiesDataThread(tbSecuritiesData, coreAlgorithmContet);
+                    Future<SecuritiesFutureVo> future = taskExecutor.submit(tbSecuritiesDataThread);
+                    futures.add(future);
+                }
+                //3、等待所有任务完成
+                List<SecuritiesFutureVo> list = new ArrayList<>();
+                for (Future<SecuritiesFutureVo> future : futures) {
+                    try {
+                        list.add(future.get());// 阻塞直到任务完成
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+                if (!CollectionUtils.isEmpty(list) && list.get(0) == null){
+                    return;
+                }
+                long endTime = System.currentTimeMillis();
+                log.debug("执行时长：{}", endTime - startTime);
+                //4、存到redis中
+                redisCache.deleteObject("securitiesFutureVoList");
+                redisCache.setCacheList("securitiesFutureVoList",list);
+            }
+        }catch (Exception e){
+            log.info("异常：",e);
+        }finally {
+            if (b){
+                lock.unlock();
+            }
+        }
     }
 
 
